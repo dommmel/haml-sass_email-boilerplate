f3ff112e34364aef52a0c10c2457862b5244cc39
Dominik <dommmel@gmail.com>
2012-12-15 16:26:46 +0100

    new (better?) folder structure

--- README.md
+++ README.md
@@ -1,12 +1,17 @@
 haml-sass_email-boilerplate
 ===========================
 
-Features
+###Features
 
 * Support for partials. Use in your haml file like so: ```%tr= partial("filename")```
 * All instance variables are available in sass too: ```background: variable(blue)```. So you can DRY up things like ```bgcolor```.
 
-How to use
+### How to use
+
 * set the variables you want to share between haml and sass in ```settings.rb```
-* compile via ```./compile.rb```
-* after development - bring styles inline
+* compile via ```./compile.rb```. This generates the html, css as well as a zip-file containing the css and images.
+* bring styles inline
+
+### Use with Campaignmonitor
+
+Upload ```index.html``` and ```assets.zip``` via the "import template" dialog and you should be set. Campaignmonitor will inline all the styles and host all refenrenced images. 
--- compile.rb
+++ compile.rb
@@ -1,8 +1,20 @@
 #! /usr/bin/env ruby
 
-require "./settings.rb"
+@build_folder = "build"
+@src_folder = "src"
+@sass_folder = "#{@src_folder}/sass"
+@haml_folder = "#{@src_folder}/haml"
+@css_folder ="#{@build_folder}/assets"
+@assets_folder ="#{@build_folder}/assets"
+@stylesheet_filename = "style"
+@markup_filename = "index"
+@input_filenames = ['images', "#{@stylesheet_filename}.css"]
+@assets_zipfile = "#{@assets_folder}.zip"
+
+require "./#{@src_folder}/settings.rb"
 require 'haml'
 require 'sass'
+require 'zip/zip'
 
 module VarAccessor
   ::Sass::Script::Functions.send :include, self
@@ -31,8 +43,9 @@ def find filename
   return filename if File.exists? filename
   filename = "./haml/"+filename
   return filename if File.exists? filename
-  p filename
-  throw "Could not find file."
+  filename = @src_folder+"/"+filename
+  return filename if File.exists? filename
+  throw "Could not find file: #{filename}"
 end
 
 def partial (filename,locals={})
@@ -41,6 +54,16 @@ def partial (filename,locals={})
   engine.render(binding,locals)
 end
 
-Sass.compile_file("sass/style.scss", "build/style.css")
-File.open("build/index.html", 'w') {|f| f.write(partial "haml/index.haml") }
-#puts partial ARGV[0]
+
+## Compile assets
+Sass.compile_file("#{@sass_folder}/#{@stylesheet_filename}.scss", "#{@assets_folder}/#{@stylesheet_filename}.css")
+File.open("#{@build_folder}/#{@markup_filename}.html", 'w') {|f| f.write(partial "#{@haml_folder}/#{@markup_filename}.haml") }
+
+# Zipping Assets
+Zip.options[:on_exists_proc] = true #overwrite if exists
+Zip.options[:continue_on_exists_proc] = true
+Zip::ZipFile.open(@assets_zipfile, Zip::ZipFile::CREATE) do |zipfile|
+  @input_filenames.each do |filename|
+    zipfile.add(filename, @assets_folder + '/' + filename)
+  end
+end
